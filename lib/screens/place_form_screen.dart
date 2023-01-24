d581e3ae915ced1faa5ddfee7c822a59d7b20afa
richard borges <richard.borges.cc@gmail.com>
2023-01-24 11:51:17 -0300

    chore: add place form scaffold and route

---
+++ lib/screens/place_form_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class PlaceFormScreen extends StatefulWidget {
+  const PlaceFormScreen({super.key});
+
+  @override
+  State<PlaceFormScreen> createState() => _PlaceFormScreenState();
+}
+
+class _PlaceFormScreenState extends State<PlaceFormScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Novo Lugar'),
+      ),
+      body: const Center(
+        child: const Text('Form'),
+      ),
+    );
+  }
+}
