4a2ef4f064042348eef9ae2671685a469bca3467
Richard Borges <richard.borges.cc@gmail.com>
2023-01-30 09:57:28 -0300

    chore: organzing image form UI

--- lib/screens/place_form_screen.dart
+++ lib/screens/place_form_screen.dart
@@ -9,6 +9,10 @@ class PlaceFormScreen extends StatefulWidget {
 }
 
 class _PlaceFormScreenState extends State<PlaceFormScreen> {
+  final _titleController = TextEditingController();
+
+  void _subimitForm() {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,16 +20,41 @@ class _PlaceFormScreenState extends State<PlaceFormScreen> {
         title: const Text('Novo Lugar'),
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'Titulo',
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  children: [
+                    TextField(
+                      controller: _titleController,
+                      decoration: InputDecoration(
+                        labelText: 'Titulo',
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
           ),
           SizedBox(
             height: 10,
           ),
-          ImageInput()
+          ImageInput(),
+          ElevatedButton.icon(
+            onPressed: _subimitForm,
+            icon: const Icon(Icons.add),
+            label: const Text(
+              'Adicionar',
+            ),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Theme.of(context).colorScheme.secondary,
+              elevation: 0,
+              tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            ),
+          ),
         ],
       ),
     );
